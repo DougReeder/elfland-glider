ef0f8442bce46c53f8212a11099b1843a93478e0
P. Douglas Reeder <reeder.29@gmail.com>
2017-12-23 16:07:01 -0500

    Adds gliding, controlled by headset roll

--- assets/state.js
+++ assets/state.js
@@ -1,17 +1,49 @@
 // state.js - state model for Elfland Glider
 // Copyright © 2017 P. Douglas Reeder
+//
+// <a-entity id="glider" bind__position="gliderPosition" bind__rotation="gliderRotation" glider-tick-state>
+//     <a-entity camera look-controls wasd-controls></a-entity>
+//     <a-entity laser-controls="hand: right"></a-entity>
+// </a-entity>
+
 
 AFRAME.registerState({
     initialState: {
+        gliderEl: null,
+        cameraEl: null,
+        time: 0,
+        gliderPosition: {x:-30, y:15, z:30},
+        gliderRotation: {x: 0, y:-45, z:0},
+        isFlying: true,
+        gliderSpeed: 5,
         stars: 0,
         questComplete: false
     },
 
     handlers: {
+        setGliderEl: function (state, gliderEl) {
+            state.gliderEl = gliderEl;
+            state.cameraEl = gliderEl.querySelector('[camera]');
+        },
+
+        iterate: function (state, action) {
+            state.time = action.time;
+            if (state.isFlying) {
+                let cameraRotation = state.cameraEl.getAttribute('rotation');
+                let deltaHeading = cameraRotation.z * action.timeDelta / 1000;
+                state.gliderRotation.y = (state.gliderRotation.y + deltaHeading + 360) % 360;
+                // console.log("cameraRotation.z", cameraRotation.z, deltaHeading, state.gliderRotation.y);
+
+                let distance = state.gliderSpeed * action.timeDelta / 1000;
+                state.gliderPosition.x += distance * Math.cos(state.gliderRotation.y/180*Math.PI);
+                state.gliderPosition.z -= distance * Math.sin(state.gliderRotation.y/180*Math.PI);
+            //     console.log(action.timeDelta, state.gliderPosition.x, deltaX);
+            }
+        },
+
         decreaseStars: function (state, action) {
             state.stars -= action.points;
         },
-
         increaseStars: function (state, action) {
             state.stars += action.points;
         }
@@ -19,5 +51,15 @@ AFRAME.registerState({
 
     computeState: function (newState, payload) {
         newState.questComplete = newState.stars >= 10;
+    }
+});
+
+AFRAME.registerComponent('glider-tick-state', {
+    init: function () {
+        AFRAME.scenes[0].emit('setGliderEl', this.el);
+    },
+
+    tick: function (time, timeDelta) {
+        AFRAME.scenes[0].emit('iterate', {time: time, timeDelta: timeDelta});
     }
 });
