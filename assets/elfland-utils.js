c24826b0abf6e9c704b0eb2034d2f333991a71e8
P. Douglas Reeder <reeder.29@gmail.com>
2018-09-22 00:36:52 -0400

    Under Chrome and mobile Safari, environmental sound is started on the first click

--- assets/elfland-utils.js
+++ assets/elfland-utils.js
@@ -28,7 +28,12 @@ function setEnvironmentalSound(url, volume) {
         autoplay: true,
         loop: true,
         volume: volume || 1.0,
-        html5: ! AFRAME.utils.device.isMobile()   // on desktop, avoids wait for full download
+        html5: ! AFRAME.utils.device.isMobile(),   // on desktop, avoids wait for full download
+        onplayerror: function() {
+            environmentalSound.once('unlock', function() {
+                environmentalSound.play();
+            });
+        }
     });
 
 }
